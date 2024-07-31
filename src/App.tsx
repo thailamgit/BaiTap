228880ff90fa2b531e9cbb3b2444754cb3b862cf
Ninh Thai Lam <thailamstudy@gmail.com>
2024-07-31 15:28:03 +0700

    Add list

--- src/App.tsx
+++ src/App.tsx
@@ -16,12 +16,27 @@ function AboutPage () {
   )
 }
 
+const products = [
+  {title: 'Carbage', id: 1},
+  {title: 'Carrot', id: 2},
+  {title: 'Corn', id: 3}
+]
+
+
 function MyApp () {
+  const listItem = products.map ((product) => 
+    <li key = {product.id}>
+      {product.title}
+    </li>
+  )
   return (
     <>
       <AboutPage/>
       <h1>This is React</h1>
       <MyButton/>
+      <ul>
+        {listItem}
+      </ul>
     </>
   )
 }
