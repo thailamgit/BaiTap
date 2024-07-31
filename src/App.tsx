8b5fdedc46a9a1253a0dc8ddcfebd7654c1d8528
Ninh Thai Lam <thailamstudy@gmail.com>
2024-07-31 14:46:19 +0700

    Add about page

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,19 @@ function MyButton () {
   )
 }
 
+function AboutPage () {
+  return (
+    <>
+      <h1>About</h1>
+      <p>Hello there. <br/> How do you do?</p>
+    </>
+  )
+}
+
 function MyApp () {
   return (
     <>
+      <AboutPage/>
       <h1>This is React</h1>
       <MyButton/>
     </>
