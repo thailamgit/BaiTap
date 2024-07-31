142c161691c44f74679a1b27785b8d3e1bd0ebf0
Ninh Thai Lam <thailamstudy@gmail.com>
2024-07-31 16:45:21 +0700

    Button display

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,18 @@
 import React from 'react';
+import { useState } from 'react';
 
-const products = [
-  {title: 'Cabbage', isFruit: false, id: 1},
-  {title: 'Apple', isFruit: true, id: 2},
-  {title: 'Banana', isFruit: true, id: 3},
-  {title: 'Garlic', isFruit: false, id: 4},
-  {title: 'Tomato', isFruit: false, id: 5},
-]
+function MyButton () {
+  const [count, setcount] = useState(0)
 
-function handleClick () {
-  alert("You clicked me!")
-}
+  function handleClick () {
+    setcount(count + 1)
+  }
 
-function shoppingList () {
-  const listItem = products.map((product) => 
-    <li key={product.id} 
-        style={{
-          color: product.isFruit ? 'magenta' : 'darkgreen'
-    }}
-    >
-      {product.title}
-    </li>
+  return (
+    <button onClick={handleClick}>
+      Clicked {count} times
+    </button>
   )
-  return <>
-    <ul>
-      {listItem}
-    </ul>
-    <button onClick={handleClick}>Click me</button>
-  </>
 }
 
-export default shoppingList
-
+export default MyButton
