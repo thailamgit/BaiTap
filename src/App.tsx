1b06ba573ba1b9b1c1a9417375aa792e8dc199eb
Ninh Thai Lam <thailamstudy@gmail.com>
2024-07-31 15:44:30 +0700

    recognize fruit or vegetable

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,24 @@
 import React from 'react';
-function MyButton () {
-  return (
-    <button>
-      I'm a button
-    </button>
-  )
-}
-
-function AboutPage () {
-  return (
-    <>
-      <h1>About</h1>
-      <p>Hello there. <br/> How do you do?</p>
-    </>
-  )
-}
 
 const products = [
-  {title: 'Carbage', id: 1},
-  {title: 'Carrot', id: 2},
-  {title: 'Corn', id: 3}
+  {title: 'Cabbage', isFruit: false, id: 1},
+  {title: 'Apple', isFruit: true, id: 2},
+  {title: 'Banana', isFruit: true, id: 3},
 ]
 
-
-function MyApp () {
-  const listItem = products.map ((product) => 
-    <li key = {product.id}>
+function ShoppingList () {
+  const listItem = products.map((product) => 
+    <li key={product.id} 
+        style={{
+          color: product.isFruit ? 'magenta' : 'darkgreen'
+    }}
+    >
       {product.title}
     </li>
   )
-  return (
-    <>
-      <AboutPage/>
-      <h1>This is React</h1>
-      <MyButton/>
-      <ul>
-        {listItem}
-      </ul>
-    </>
-  )
+  return <ul>
+    {listItem}
+  </ul>
 }
 
-export default MyApp
+export default ShoppingList
