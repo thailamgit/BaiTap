58cb0e44c37ab88f90dd8d87aa2e6fa200eef9d1
Ninh Thai Lam <thailamstudy@gmail.com>
2024-07-31 18:06:44 +0700

    Sync button click

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
 import React from 'react';
 import { useState } from 'react';
 
-function MyButton () {
+function MyApp () {
   const [count, setcount] = useState(0)
-
   function handleClick () {
     setcount(count + 1)
   }
-
-  return (
-    <button onClick={handleClick}>Clicked {count} times!</button>
-  )
-}
-
-function MyApp () {
   return (
     <div>
       <h1>Counters that update separately</h1>
-      <MyButton/><br/>
-      <MyButton/>
+      <MyButton count={count} onClick={handleClick}/><br/>
+      <MyButton count={count} onClick={handleClick}/>
     </div>
+  )
+}
+
+type MyButtonProps = {
+  count: number
+  onClick: () => void
+}
+
+function MyButton ({count, onClick}: MyButtonProps) {
+  return (
+    <button onClick={onClick}>
+      Clicked {count} times
+    </button>
   )
 }
 export default MyApp
